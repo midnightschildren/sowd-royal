4f495f59486ed56dfe1a3c66257a03d65e53d1dc
midnightschildren <amar@codisattva.com>
2016-08-16 23:51:34 -0700

    Review styles + Consult Page

--- page-templates/consult-page.php
+++ page-templates/consult-page.php
@@ -13,11 +13,76 @@ get_header(); ?>
 
 <?php
 global $product;
-if(is_user_logged_in() && has_bought()) {
+if(is_user_logged_in() && has_bought()) { ?>
 	 
-	echo '<p>test - bought item</p>';
+		<div id="primary" class="site-content row" role="main">
 
- };
+	<div class="grid-12">
+		<?php if ( function_exists( 'envira_gallery' ) ) { envira_gallery( '78' ); } ?>
+	</div>	
+	
+
+	</div><!-- /#primary.site-content.row -->
+
+	<div id="services" class="about-content row">
+
+	<div class="grid-12 pad-3-vert">	
+
+		<?php if ( have_posts() ) : ?>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<div class="grid-12 pad-3-vert">
+			
+	
+			<h5 class="center medium hsslate"><?php the_title(); ?></h5>
+	
+			
+		</div>	
+		<div class="grid-8 offset-2 pad-3-vert">
+
+			<?php the_content(); ?>	
+
+		</div>
+
+	</div>
+
+		<?php endwhile; // end of the loop. ?>
+
+		<?php endif; // end have_posts() check ?>
+		
+	</div>	
+
+	<div class="about_hannah-content row">
+
+		<div class="grid-12 pad-3-vert">
+
+			<div class="grid-12 pad-3-vert">
+
+				<?php if( get_field('about_section_title') ): ?>
+	
+					<h5 class="center bluegreen pad-2-vert"><?php the_field('about_section_title'); ?></h5>
+	
+				<?php endif; ?>
+
+			</div>
+
+		<div class="grid-8 offset-2 pad-3-vert">
+
+			<?php if( get_field('about_section_content') ): ?>
+	
+			<?php the_field('about_section_content'); ?>
+	
+			<?php endif; ?>		
+
+		</div>	
+
+		</div>
+
+	</div>			
+
+
+<?php };
 
 if(is_user_logged_in() && !has_bought()) { ?>
 

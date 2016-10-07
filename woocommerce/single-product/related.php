9da5dd1b73f3a439600c704f28e9b65f992e355e
midnightschildren <amar@codisattva.com>
2016-10-07 14:47:56 -0700

    Done! Done!

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -46,6 +46,7 @@ $woocommerce_loop['columns'] = apply_filters( 'woocommerce_related_products_colu
 
 if ( $products->have_posts() ) : ?>
 
+<div class="related-content row">
 	<div class="related products pad-3-bottom pad-4-top grid-10 s-grid-12 offset-1 s-offset-0 s-pad-3-sides">
 
 		<h5 class="center hsslate pad-2-vert"><?php _e( 'Related Products', 'woocommerce' ); ?></h5>
@@ -61,7 +62,7 @@ if ( $products->have_posts() ) : ?>
 		<?php woocommerce_product_loop_end(); ?>
 
 	</div>
-
+</div>
 <?php endif;
 
 wp_reset_postdata();
